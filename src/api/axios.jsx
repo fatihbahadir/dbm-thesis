8222c1642401620d0423822cfa4de98bbc5be4a6
fatihbahadir <hunkarhyme@gmail.com>
2024-01-06 19:06:42 +0300

    url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080'
-//  https://dms-thesis.up.railway.app/
+const BASE_URL = 'https://dms-thesis.up.railway.app/'
+// http://localhost:8080
 export default axios.create({
     baseURL: BASE_URL,
 })
